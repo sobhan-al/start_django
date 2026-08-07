483f599cbf49303426e9ab5f7730c971bb766adb
sobhan-al <alavi.s2001@gmail.com>
2026-08-07 14:22:15 +0330

    created first app named website with tree views contact,about,index

--- sbsite/urls.py
+++ sbsite/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from sbsite.views import http_test , json_test
-
+from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('http-res',http_test),
-    path('json-res',json_test)
+    path('', include('website.urls'))
 ]
--- sbsite/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-
-def http_test(request):
-    return HttpResponse('<h1>This is a first test</h1>')
-
-def json_test(requests):
-    return JsonResponse({'name' : 'ali'})
-
