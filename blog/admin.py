b0be80057b41d4232dbfbe7caae6b0b07cf9e171
sobhan-al <alavi.s2001@gmail.com>
2026-08-30 15:30:50 +0330

    added author field and image field and category field to post table. created category table. created blog-writer.html to include in another templates.

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Category
 
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
-    list_display = ['id','title' , 'counted_views' , 'status' , 'created_date' , 'publish_date']
+    list_display = ['id','title' , 'author', 'counted_views' , 'status' , 'created_date' , 'publish_date']
     empty_value_display = '-empty-'
     list_filter = ['status']
     search_fields = ['title','content']
 
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
-    #image
-    #author
+    image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
+    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     #tag
-    #category
+    categorys = models.ManyToManyField(Category)
     counted_views = models.PositiveIntegerField(default=0)
     status = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     publish_date = models.DateTimeField(null=True)
+
 
     class Meta:
         ordering = ['created_date']
