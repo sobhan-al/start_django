3b1723dc7d7b43adec0805d43381d012e5131948
sobhan-al <alavi.s2001@gmail.com>
2026-08-30 18:32:27 +0330

    created ads,comments,category,newsletter,popularpost html files to use them with include template tag in many pages.

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255,unique=True)
+    related_name='posts'
     def __str__(self):
         return self.name
 
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render,get_object_or_404
-from blog.models import Post
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Count
+
+from blog.models import Post, Category
 
 
 def blog_view(request):
+
     posts = Post.objects.all()
-    context = {'posts':posts}
+    categoryss = Category.objects.all()
 
-    return render(request,'blog/blog-home.html',context)
+    for category in categoryss:
+        category.post_count = Post.objects.filter(
+            categorys=category
+        ).count()
 
+    context = {
+        'posts': posts,
+        'categoryss': categoryss,
+    }
 
-def blog_single(request,pid):
-    post = get_object_or_404(Post,pk=pid)
-    context = {'post':post}
-    return render(request,'blog/blog-single.html',context)
+    return render(request, 'blog/blog-home.html', context)
 
+def blog_single(request, pid):
 
+    post = get_object_or_404(Post, pk=pid)
+
+    posts = Post.objects.all()
+    categoryss = Category.objects.all()
+
+    for category in categoryss:
+        category.post_count = Post.objects.filter(
+            categorys=category
+        ).count()
+
+    context = {
+        'post': post,
+        'posts': posts,
+        'categoryss': categoryss,
+    }
+
+    return render(request, 'blog/blog-single.html', context)
