675a3c3af8c823e2156f37fadfc604c709b8444c
sobhan-al <alavi.s2001@gmail.com>
2026-09-08 14:44:30 +0330

    created paginator in blog_view and complited it in blog-home.html

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
 from django.db.models import Q
 from blog.models import Post, Category
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def blog_view(request,name=None,author_username=None):
     posts = Post.objects.filter(status=1)
@@ -10,6 +10,16 @@ def blog_view(request,name=None,author_username=None):
         posts = posts.filter(author__username=author_username)
     if name:
         posts = posts.filter(categorys__name=name)
+
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.page(posts.num_pages)
+            
     context = {
         'posts': posts,
     }
@@ -29,7 +39,7 @@ def blog_single(request, pid):
 def blog_search(request):
     posts = Post.objects.filter(status=1)
     if request.method == 'GET':
-        posts = posts.filter(Q(content__contains=request.GET.get('s')) | Q(title__contains=request.GET.get('s')))
+        posts = posts.filter(Q(content__contains=request.GET.get('s')) | Q(title__contains=request.GET.get('s')) | Q(categorys__name__contains=request.GET.get('s')))
     context = {
         'posts': posts
     }
